2e3851c22bb7488039f84b106d1b1a647d5dfbe0
U-GINA-PC\gina <ginabythebay@gmail.com>
2018-10-19 17:01:07 -0700

    fix win32 keybindings

--- hosts/init-Gina-PC.el
+++ hosts/init-Gina-PC.el
@@ -28,11 +28,10 @@
 (pr-update-menus t)
 
 ;; See http://ergoemacs.org/emacs/emacs_hyper_super_keys.html
-(setq w32-pass-lwindow-to-system nil)
-w32-lwindow-modifier 'super ; Left Windows key
-
-setq w32-pass-rwindow-to-system nil
-setq w32-rwindow-modifier 'super ; Right Windows key
+(setq w32-pass-lwindow-to-system nil
+      w32-lwindow-modifier 'super ; Left Windows key
+      w32-pass-rwindow-to-system nil
+      w32-rwindow-modifier 'super) ; Right Windows key
 
 ;;(setq w32-pass-apps-to-system nil)
 ;;(setq w32-apps-modifier 'hyper) ; Menu/App key
