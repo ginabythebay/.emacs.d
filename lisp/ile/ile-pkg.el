dfe1b7f69feff28e0f68aa8af2a499f6569dffee
U-GINA-PC\gina <ginabythebay@gmail.com>
2018-11-07 08:16:40 -0800

    provide comment chrome for pkg def

--- lisp/ile/ile-pkg.el
+++ lisp/ile/ile-pkg.el
@@ -1,1 +1,18 @@
+;;; ile-pkg.el --- tests for bates.el -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018  Gina White
+
+;; Author: Gina White <ginabythebay@gmail.com>
+
+;;; Commentary:
+
+;; package definition for ile
+
+;;; Code:
+
+;; code goes here
 (define-package "ile" "0.0.1")
+
+(provide 'ile-pkg)
+;;; ile-pkg.el ends here
+
