2ad598dd2520289c55a7d56a15b6baa32b999333
U-GINA-PC\gina <ginabythebay@gmail.com>
2018-11-09 15:56:44 -0800

    add launcher entry for bury-buffer

--- lisp/gina-launcher.el
+++ lisp/gina-launcher.el
@@ -38,11 +38,13 @@
 (defhydra gina-launcher-hydra (:hint nil :exit t)
   "
 (quit with _q_)
+_b_: bury buffer
 _d_: toggle debug on error
 _t_: open Time sheet
 _i_: clock In to recent task
 _c_: jump to Current clock"
   ("q" nil)
+  ("b" bury-buffer)
   ("d" toggle-debug-on-error)
   ("t" gina-open-current-timesheet)
   ("i" gina-clock-in-to-recent-task)
