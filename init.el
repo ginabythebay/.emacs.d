2ceb570060845cd8b5b5042b0f109c02107101d8
U-GINA-PC\gina <ginabythebay@gmail.com>
2018-11-14 08:21:48 -0800

    remove undo-tree.
    I never really used it and it was crashing when I tried to open a new
    frame.

--- init.el
+++ init.el
@@ -210,13 +210,6 @@ the syntax class ')'."
 ;; Our default, for now
 (load-theme 'tangotango)
 
-(use-package undo-tree
-  :ensure t
-  :config
-  (global-undo-tree-mode)
-  (setq undo-tree-visualizer-timestamps t)
-  :diminish undo-tree-mode)
-
 ;; Project management.
 (use-package projectile
   :ensure t
