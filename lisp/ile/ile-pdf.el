8b429897863752b34fef2f7921074f79513f6cd8
U-GINA-PC\gina <ginabythebay@gmail.com>
2018-11-19 11:34:06 -0800

    ile-pdf-from-html, first draft

--- lisp/ile/ile-pdf.el
+++ lisp/ile/ile-pdf.el
@@ -22,7 +22,12 @@
 
 (defcustom ile-pdf-pdftk (executable-find "pdftk")
   "Path to pdftk executable."
-  :group 'ile-pdf-separate
+  :group 'ile-pdf
+  :type 'string)
+
+(defcustom ile-pdf-wkhtmltopdf (executable-find "wkhtmltopdf")
+  "Path to wkhtmltopdf executable."
+  :group 'ile-pdf
   :type 'string)
 
 (defconst ile-pdf--output-buffer "*ile-pdf*")
@@ -50,6 +55,40 @@ page."
             "foo"
             '( (1 . 13) (15 . 20))
             "bar"))))
+
+(defun ile-pdf-from-html (in-files out-file &optional footer-left footer-right)
+  "Convert one or more html files to a pdf file.
+
+IN-FILES is either a single html file or a list of html files.
+OUT-FILE is the pdf file to output to.  if FOOTER-LEFT or
+FOOTER-RIGHT is set, we will pass --footer-line to the
+executable."
+  (when (get-buffer ile-pdf--output-buffer)
+    (kill-buffer ile-pdf--output-buffer))
+
+  (let ((args
+         '("--orientation" "Landscape"
+           "--no-background")))
+    (when footer-left
+      (setq args (append args `("--footer-left" ,footer-left))))
+    (when footer-right
+      (setq args (append args `("--footer-right" ,footer-right))))
+    (when (or footer-left footer-right)
+      (setq args (append args '("footer-line"))))
+    (when (not (listp in-files))
+      (setq in-files (list in-files)))
+    (setq args (append args in-files (list out-file)))
+
+    (let ((status (apply
+                   #'call-process
+                   ile-pdf-wkhtmltopdf
+                   nil
+                   ile-pdf--output-buffer
+                   nil
+                   args)))
+      (unless (equal status 0)
+        (error "Failed convert to pdf.  See buffer %s for more detail "
+               ile-pdf--output-buffer)))))
 
 (defun ile-pdf--extract-pages (in-file page-ranges out-file)
   "Extract one or more pages from IN-FILE and put them into OUT-FILE.
