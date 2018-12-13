317671f6e29f6a24babde90c3f2381d5ec53d0b3
U-GINA-PC\gina <ginabythebay@gmail.com>
2018-12-13 09:15:05 -0800

    Add marec mode, keybinding to update

--- lisp/marec.el
+++ lisp/marec.el
@@ -18,12 +18,20 @@
 (defun marec-status ()
   "Look at open buffers potentially needing recovery."
   (interactive)
+  (let ((buf (marec-update)))
+    (with-current-buffer (marec-update)
+      (marec-mode))
+    (pop-to-buffer buf)))
+
+(defun marec-update ()
+  "Update the marec buffer.
+Returns the status buffer."
+  (interactive)
   (let* ((inhibit-read-only t)
          (candidates (seq-filter #'marec-buf-candidate-p (buffer-list)))
          (candidates-p (not (zerop (length candidates))))
          (status-buffer (get-buffer-create "*marec status*")))
     (with-current-buffer status-buffer
-      (setq buffer-read-only t)
       (erase-buffer)
       (if (not candidates-p)
           (insert "No files recoverable buffers open")
@@ -32,27 +40,37 @@
                  do
                  (newline)
                  (insert "  " (buffer-name c))))
-
-      (when candidates-p
-        (pop-to-buffer status-buffer))
-
       (if candidates-p
           (message "%d buffers appear to be recoverable" (length candidates))
-        (message "No buffers appear to be recoverable"))
-      )
-    )
-  )
+        (message "No buffers appear to be recoverable")))
+    status-buffer))
 
+(defalias #'marec-status 'marec)
 
 (defun marec-buf-candidate-p (buf)
   "Return non-nil if BUF is a candidate for recovery.
 Based on code in `after-find-file'"
   (with-current-buffer buf
-    (when (and buffer-file-name (file-writable-p buffer-file-name))
-      (file-newer-than-file-p (or buffer-auto-save-file-name
-				  (make-auto-save-file-name))
-			      buffer-file-name))))
+    (and
+     buffer-file-name
+     (file-writable-p buffer-file-name)
+     (not (buffer-base-buffer))   ;; exclude indirect buffers
+     (file-newer-than-file-p (or buffer-auto-save-file-name
+				 (make-auto-save-file-name))
+			     buffer-file-name))))
+
+(defvar marec-mode-map
+  (let ((map (make-keymap)))
+    (define-key map (kbd "g") #'marec-update)
+    map))
 
 
-(provide 'marec)
+(define-derived-mode marec-mode special-mode "Marec"
+  "A major mode for magic recovery."
+  (setq buffer-read-only t)
+  (buffer-disable-undo)
+  (set (make-local-variable 'revert-buffer-function)
+       #'marec-update))
+
+  (provide 'marec)
 ;;; marec.el ends here
