43204941062fff600db83f4943318b8af35fb5a4
Gina White <ginabythebay@gmail.com>
2018-12-29 15:58:29 -0800

    ile-case-planner
    checkpoint b4 we try refactoring to be more lispy

--- lisp/ile/ile.el
+++ lisp/ile/ile.el
@@ -108,6 +108,75 @@ See `org-sort-entries'."
   (interactive)
   (org-sort-entries nil ?f #'ile-entry-when-sort-key #'string<))
 
+(defconst ile-case-planner-buffer-name "*ile case planner*")
+
+;;;###autoload
+(defun ile-case-planner (cmd-key)
+  "Run an agenda command in batch mode and send the result to STDOUT.
+If CMD-KEY is a string of length 1, it is used as a key in
+`org-agenda-custom-commands' and triggers this command.  If it is a
+longer string it is used as a tags/todo match string.
+Parameters are alternating variable names and values that will be bound
+before running the agenda command.
+
+The output gives a line for each selected agenda item.  Each
+item is a list of comma-separated values, like this:
+
+category,head,type,todo,tags,date,time,extra,priority-l,priority-n
+
+category     The category of the item
+head         The headline, without TODO kwd, TAGS and PRIORITY
+type         The type of the agenda entry, can be
+                todo               selected in TODO match
+                tagsmatch          selected in tags match
+                diary              imported from diary
+                deadline           a deadline on given date
+                scheduled          scheduled on given date
+                timestamp          entry has timestamp on given date
+                closed             entry was closed on given date
+                upcoming-deadline  warning about deadline
+                past-scheduled     forwarded scheduled item
+                block              entry has date block including g. date
+todo         The todo keyword, if any
+tags         All tags including inherited ones, separated by colons
+date         The relevant date, like 2007-2-14
+time         The time, like 15:00-16:50
+extra        Sting with extra planning info
+priority-l   The priority letter if any was given
+priority-n   The computed numerical priority
+agenda-day   The day in the agenda where this is listed"
+  (interactive "MOrg Cmd: ")
+  ;; (if (> (length cmd-key) 2)
+  ;;     (org-tags-view nil cmd-key)
+  ;;   (let ((org-agenda-span 700))
+  ;;     (org-agenda nil cmd-key)))
+  (set-buffer org-agenda-buffer-name)
+  (let* ((lines (org-split-string (buffer-string) "\n"))
+	 line cases case-names)
+    (while (setq line (pop lines))
+      (catch 'next
+	(if (not (get-text-property 0 'org-category line)) (throw 'next nil))
+        (let* ((props (org-fix-agenda-info (text-properties-at 0 line)))
+               (category (plist-get props 'org-category))
+               (events (assoc-default category cases 'string= '())))
+          (setq cases (cons (cons category (append events (list props))) cases)))))
+    (cl-delete-duplicates cases :key #'car :test #'string= :from-end t)
+    (setq case-names (sort (mapcar 'car cases) #'string<))
+    (switch-to-buffer ile-case-planner-buffer-name nil t)
+    (erase-buffer)
+    ;; TODO (gina) make buffer read only;  inhibit it when we need to right
+    ;; TODO (gina) allow 'g' to refresh the buffer
+    (org-mode)
+    (insert "* Case Planner\n")
+    (insert "Generated " (format-time-string "%B %d, %Y at %I:%M:%S %p") "\n")
+    (let (cat-set (mapcar )
+                  (last-cat))
+      (dolist (name case-names)
+        (insert "** " name "\n")
+        )
+      )))
+
+
 (provide 'ile)
 ;;; ile.el ends here
 
