4acba5d1a4123ba85bbb82ff42113ea0b4ebbcf6
U-GINA-PC\gina <ginabythebay@gmail.com>
2019-03-20 12:41:23 -0700

    support time ranges in clock code

--- lisp/ile/ile-clock-tests.el
+++ lisp/ile/ile-clock-tests.el
@@ -17,6 +17,8 @@
 
 (require 'ile-clock)
 (require 'ert)
+(require 'org)
+(require 'org-clock)
 
 (ert-deftest ile-clock--minutes-to-duration ()
   "Tests time conversion"
@@ -30,22 +32,36 @@
 
 (ert-deftest ile-clock--parse-clock ()
   "Test clock parsing."
-  (with-temp-buffer
-    (insert "* TC with Client
+
+  (let* ((rstart 0.0)
+         (rend (float-time
+                (apply #'encode-time
+                       (org-parse-time-string "2019-03-20 Wed 09:17"))))
+         (fn (lambda (e) (ile-clock--parse-clock rstart rend e))))
+    (with-temp-buffer
+      (insert "* TC with Client
    :LOGBOOK:
    CLOCK: [2019-03-15 Fri 12:02]--[2019-03-15 Fri 12:09] =>  0:07")
-    (should (equal (org-element-map (org-element-parse-buffer) 'clock
-                     #'ile-clock--parse-clock nil nil)
-                   '((:task "TC with Client"
-                            :date "2019-03-15"
-                            :minutes 7)))))
-  ;; test that clock entries spanning dates raise an error
-  (with-temp-buffer
-    (insert "* TC with Client
+      (should (equal (org-element-map (org-element-parse-buffer)
+                         'clock fn nil nil)
+                     '((:task "TC with Client"
+                              :date "2019-03-15"
+                              :minutes 7)))))
+    ;; outside the range
+    (with-temp-buffer
+      (insert "* TC with Client
+   :LOGBOOK:
+   CLOCK: [2019-03-22 Fri 12:02]--[2019-03-22 Fri 12:10] =>  0:08")
+      (should (equal (org-element-map (org-element-parse-buffer)
+                         'clock fn nil nil)
+                     nil)))
+    ;; test that clock entries spanning dates raise an error
+    (with-temp-buffer
+      (insert "* TC with Client
    :LOGBOOK:
    CLOCK: [2019-03-14 Thu 12:02]--[2019-03-15 Fri 12:09] =>  24:07")
-    (should-error (org-element-map (org-element-parse-buffer) 'clock
-                    #'ile-clock--parse-clock nil nil))))
+      (should-error (org-element-map (org-element-parse-buffer)
+                        'clock fn nil nil)))))
 
 (ert-deftest ile-clock--combine-entries ()
   "Test clock combination"
@@ -73,16 +89,29 @@
    :LOGBOOK:
    CLOCK: [2019-03-13 Thu 12:02]--[2019-03-13 Thu 12:09] =>  0:08
    :END:")
+    ;; with no range specified
     (should (equal (ile-clock-entries)
-             '((:task "TC with Client"
-                      :date "2019-03-12"
-                      :minutes 24)
-               (:task "Analyze docs"
-                      :date "2019-03-13"
-                      :minutes 8)
-               (:task "TC with Client"
-                      :date "2019-03-15"
-                      :minutes 67))))))
+                   '((:task "TC with Client"
+                            :date "2019-03-12"
+                            :minutes 24)
+                     (:task "Analyze docs"
+                            :date "2019-03-13"
+                            :minutes 8)
+                     (:task "TC with Client"
+                            :date "2019-03-15"
+                            :minutes 67))))
+    ;; test specifying a range
+    (let ((rstart (apply #'encode-time
+                       (org-parse-time-string "2019-01-01 Fri 09:17")))
+          (rend (apply #'encode-time
+                       (org-parse-time-string "2019-03-14 Fri 09:17"))))
+      (should (equal (ile-clock-entries (list rstart rend))
+                     '((:task "TC with Client"
+                              :date "2019-03-12"
+                              :minutes 24)
+                       (:task "Analyze docs"
+                              :date "2019-03-13"
+                              :minutes 8)))))))
 
 ;; ile-clock-test-current-file
 
--- lisp/ile/ile-clock.el
+++ lisp/ile/ile-clock.el
@@ -15,11 +15,28 @@
 ;; code goes here
 
 (require 'org)
+(require 'org-clock)
 (require 'seq)
 (require 'subr-x)
 
-(defun ile-clock--parse-clock (element)
-  "Ingest clock ELEMENT and produces a plist of its relevant properties."
+(defun ile-clock--in-rangep (rstart rend timestamp)
+  "Decides if TIMESTAMP overlaps with a range.
+
+TIMESTAMP is a string like
+\"[2019-03-12 Wed 12:02]--[2019-03-12 Wed 12:09]\",
+and RSTART and REND are float times."
+  (let* ((tokens (split-string timestamp "--"))
+         (tstart (float-time
+                  (apply #'encode-time (org-parse-time-string (car tokens)))))
+         (tend (float-time
+                (apply #'encode-time (org-parse-time-string (cadr tokens))))))
+    (and (<= rstart tstart) (<= tend rend))))
+
+(defun ile-clock--parse-clock (rstart rend element)
+  "Ingest clock ELEMENT and produces a plist of its relevant properties.
+
+RSTART and REND represent a range of times and we only return clock entries
+that overlap with that range."
   (when (and (equal (org-element-type element) 'clock)
              ;; Only ingest closed, inactive clock elements.
              (equal (org-element-property :status element) 'closed)
@@ -32,8 +49,8 @@
                         (org-element-property :month-start timestamp)
                         (org-element-property :day-start timestamp)))
            (end (list (org-element-property :year-end timestamp)
-                        (org-element-property :month-end timestamp)
-                        (org-element-property :day-end timestamp)))
+                      (org-element-property :month-end timestamp)
+                      (org-element-property :day-end timestamp)))
            (task (org-element-property :raw-value (org-element-lineage element '(headline)))))
       (unless (equal start end)
         (user-error
@@ -41,10 +58,13 @@
          task
          (apply #'format "%d-%02d-%02d" start)
          (apply #'format "%d-%02d-%02d" end)))
-      (list
-       :task task
-       :date (apply #'format "%d-%02d-%02d" start)
-       :minutes (ile-clock--duration-to-minutes duration)))))
+      (when (ile-clock--in-rangep rstart
+                                  rend
+                                  (org-element-property :raw-value timestamp))
+        (list
+         :task task
+         :date (apply #'format "%d-%02d-%02d" start)
+         :minutes (ile-clock--duration-to-minutes duration))))))
 
 (defun ile-clock--duration-to-minutes (d)
   "Convert a duration D like \"1:23\" to 83."
@@ -74,23 +94,35 @@ Assumes that all ENTRIES have the same task and start."
 ;;   - expand this to optionally take a list of files to draw from.
 ;;   - Also allow it to accept a value like 'lastweek to narrow the results.  See
 ;;      org-clock-special-range
-(defun ile-clock-entries ()
-  "Get clock entries for the current buffer."
-  (sort
-   ;; list with one entry per task/date, with times summed up
-   (cl-loop for e in
-            ;; alist where entries are grouped by task and date
-            (seq-group-by
-             (lambda (e) (list (plist-get e :task) (plist-get e :date)))
-             (org-element-map (org-element-parse-buffer) 'clock
-               #'ile-clock--parse-clock nil nil))
-            collect (ile-clock--combine-entries (cdr e)))
-   (lambda (a b)
-     (let ((a-date (plist-get a :date))
-           (b-date (plist-get b :date)))
-       (or (string< a-date b-date)
-           (and (string= a-date b-date)
-                (string< (plist-get a :task) (plist-get b :task))))))))
+(defun ile-clock-entries (&optional range)
+  "Get clock entries for the current buffer.
+
+RANGE specifies the start time and end time to include in float
+time.  We expect a list where the car is start time and the cadr
+is the end time, as returned by `org-clock-special-range'.  If not
+specified, we will include all entries."
+  (let* ((rstart (if range
+                     (float-time (car range))
+                   0))
+         (rend (if range
+                   (float-time (cadr range))
+                 (float-time (current-time))))
+         (parser (lambda (e) (ile-clock--parse-clock rstart rend e))))
+    (sort
+     ;; list with one entry per task/date, with times summed up
+     (cl-loop for e in
+              ;; alist where entries are grouped by task and date
+              (seq-group-by
+               (lambda (e) (list (plist-get e :task) (plist-get e :date)))
+               (org-element-map (org-element-parse-buffer) 'clock
+                 parser nil nil))
+              collect (ile-clock--combine-entries (cdr e)))
+     (lambda (a b)
+       (let ((a-date (plist-get a :date))
+             (b-date (plist-get b :date)))
+         (or (string< a-date b-date)
+             (and (string= a-date b-date)
+                  (string< (plist-get a :task) (plist-get b :task)))))))))
 
 (defun ile-clock-test-current-file ()
   "Test of clock parser."
