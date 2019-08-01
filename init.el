bb50e1a6ff6498876fcf0af2714875ac3c5406ff
U-GINA-PC\gina <ginabythebay@gmail.com>
2019-08-01 11:57:48 -0700

    make opening in desktop work with dired-subtree

--- init.el
+++ init.el
@@ -263,41 +263,6 @@ the syntax class ')'."
 	      ("m" . mc/edit-lines)
 	      ("a" . mc/mark-all-like-this))))
 
-;; from http://ergoemacs.org/emacs/emacs_dired_open_file_in_ext_apps.html
-(defun xah-show-in-desktop ()
-  "Show current file in desktop.
-\\(Mac Finder, Windows Explorer, Linux file manager)
-This command can be called when in a file or in `dired'.
-
-URL `http://ergoemacs.org/emacs/emacs_dired_open_file_in_ext_apps.html'
-Version 2018-09-29"
-  (interactive)
-  (let (($path (if (buffer-file-name) (buffer-file-name) default-directory )))
-    (cond
-     ((string-equal system-type "windows-nt")
-      (w32-shell-execute "explore" (replace-regexp-in-string "/" "\\" $path t t)))
-     ((string-equal system-type "darwin")
-      (if (eq major-mode 'dired-mode)
-          (let (($files (dired-get-marked-files )))
-            (if (eq (length $files) 0)
-                (progn
-                  (shell-command
-                   (concat "open " default-directory)))
-              (progn
-                (shell-command
-                 (concat "open -R " (shell-quote-argument (car (dired-get-marked-files ))))))))
-        (shell-command
-         (concat "open -R " $path))))
-     ((string-equal system-type "gnu/linux")
-      (let (
-            (process-connection-type nil)
-            (openFileProgram (if (file-exists-p "/usr/bin/gvfs-open")
-                                 "/usr/bin/gvfs-open"
-                               "/usr/bin/xdg-open")))
-        (start-process "" nil openFileProgram $path))
-      ;; (shell-command "xdg-open .") ;; 2013-02-10 this sometimes froze emacs till the folder is closed. eg with nautilus
-      ))))
-
 (use-package dired
   :ensure nil
   :init
@@ -332,6 +297,46 @@ Version 2018-09-29"
   (bind-keys :map dired-mode-map
              ("i" . dired-subtree-insert)
              (";" . dired-subtree-remove)))
+
+
+;; from http://ergoemacs.org/emacs/emacs_dired_open_file_in_ext_apps.html
+(defun xah-show-in-desktop ()
+  "Show current file in desktop.
+\\(Mac Finder, Windows Explorer, Linux file manager)
+This command can be called when in a file or in `dired'.
+
+URL `http://ergoemacs.org/emacs/emacs_dired_open_file_in_ext_apps.html'
+Version 2018-09-29"
+  (interactive)
+  (let (($path (if (buffer-file-name) (buffer-file-name) default-directory )))
+    (cond
+     ((string-equal system-type "windows-nt")
+      (let ((subtree-ov (dired-subtree--get-ov)))
+        (when subtree-ov
+          (setq $path (overlay-get subtree-ov 'dired-subtree-name))))
+      (w32-shell-execute "explore" (replace-regexp-in-string "/" "\\" $path t t)))
+     ((string-equal system-type "darwin")
+      (if (eq major-mode 'dired-mode)
+          (let (($files (dired-get-marked-files )))
+            (if (eq (length $files) 0)
+                (progn
+                  (shell-command
+                   (concat "open " default-directory)))
+              (progn
+                (shell-command
+                 (concat "open -R " (shell-quote-argument (car (dired-get-marked-files ))))))))
+        (shell-command
+         (concat "open -R " $path))))
+     ((string-equal system-type "gnu/linux")
+      (let (
+            (process-connection-type nil)
+            (openFileProgram (if (file-exists-p "/usr/bin/gvfs-open")
+                                 "/usr/bin/gvfs-open"
+                               "/usr/bin/xdg-open")))
+        (start-process "" nil openFileProgram $path))
+      ;; (shell-command "xdg-open .") ;; 2013-02-10 this sometimes froze emacs till the folder is closed. eg with nautilus
+      ))))
+
 
 (use-package ibuffer-projectile
   :ensure t
