4903f17fcdd1b5bd54c848a30042a6acdc3af080
Gina White <ginabythebay@gmail.com>
2021-05-01 10:05:50 -0700

    first crack at gina-open-default-buffers

--- lisp/gina-launcher.el
+++ lisp/gina-launcher.el
@@ -18,23 +18,38 @@
 (when (string= system-type "windows-nt")
   (require 'w32-browser))
 
-(defun gina-refile ()
-  "Open a new frame for filing things."
+(defun gina-open-shell-quietly (buffer-name)
+  "If a shell with BUFFER-NAME doesn't exist, opens it with now window."
+  (unless (get-buffer buffer-name)
+    (let ((display-buffer-overriding-action '((display-buffer-same-window))))
+      (shell buffer-name)
+      (bury-buffer))))
+
+(defun gina-open-default-buffers ()
+  "Open some default buffers I usually want."
   (interactive)
-  (select-frame-set-input-focus
-   (make-frame `((fullscreen . maximized))))
+  (dired "~/scan")
+  (bury-buffer)
+  (gina-open-shell-quietly "/sys")
+  (gina-open-shell-quietly "/scratch"))
 
-  (split-window-right)
+  (defun gina-refile ()
+    "Open a new frame for filing things."
+    (interactive)
+    (select-frame-set-input-focus
+     (make-frame `((fullscreen . maximized))))
 
-  (dired "c:/Users/gina/Documents/Downloads")
-  (dired-sort-other "-lt")              ; also refreshes
-  (dired "c:/Users/gina/Documents/Clare Lacy/ScannedDocuments")
-  (dired-sort-other "-lt")              ; also refreshes
+    (split-window-right)
 
-  (other-window 1)
-  (dired "c:/Users/gina/Documents/Gina")
+    (dired "c:/Users/gina/Documents/Downloads")
+    (dired-sort-other "-lt")            ; also refreshes
+    (dired "c:/Users/gina/Documents/Clare Lacy/ScannedDocuments")
+    (dired-sort-other "-lt")            ; also refreshes
 
-  (other-window 1))
+    (other-window 1)
+    (dired "c:/Users/gina/Documents/Gina")
+
+    (other-window 1))
 
 (provide 'gina-launcher)
 
@@ -42,14 +57,16 @@
   "
 (quit with _q_)
 _b_: back (pop global mark)
-_d_: toggle debug on error
+_d_: open default buffers
+_e_: toggle debug on error
 _s_: iSearch forward
 _u_: bury buffer
 _v_: toggle visual line mode
 "
   ("q" nil)
   ("b" pop-global-mark :exit nil)
-  ("d" toggle-debug-on-error)
+  ("d" gina-open-default-buffers)
+  ("e" toggle-debug-on-error)
   ("i" gina-clock-in-to-recent-task)
   ("s" isearch-forward)
   ("u" bury-buffer)
