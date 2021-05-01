11025cbf9bb75b128aede36b62cacc0b702f5212
Gina White <ginabythebay@gmail.com>
2021-05-01 06:51:25 -0700

    clean out unused launcher stuff

--- lisp/gina-launcher.el
+++ lisp/gina-launcher.el
@@ -18,28 +18,10 @@
 (when (string= system-type "windows-nt")
   (require 'w32-browser))
 
-(defvar timesheet-dir "c:/Users/gina/Documents/Gina/Noncases/timesheets/")
-
 (defun gina-clock-in-to-recent-task ()
   "Prompt the user for a recent task and clock in to it."
   (interactive)
   (org-clock-in '(4)))
-
-(defun gina-open-current-timesheet ()
-  "Open the current time sheet.  Assumes windows."
-  (interactive)
-  (let ((candidates (directory-files timesheet-dir t "Time Sheet")))
-    (unless (eq (length candidates) 1)
-      (user-error
-       "Expected to find exactly one timesheet in %S but found %S"
-       timesheet-dir
-       (pp-to-string candidates)))
-    (w32-browser (car candidates))))
-
-(defun gina-open-case-list ()
-  "Open the case list."
-  (interactive)
-  (w32-browser "c:/Users/gina/Documents/Gina/CaseList.doc"))
 
 (defun gina-refile ()
   "Open a new frame for filing things."
@@ -59,15 +41,6 @@
 
   (other-window 1))
 
-(defun gina-open-explorer ()
-  "Open windows explorer in the director for the buffer."
-  (interactive)
-  (if (eq major-mode 'dired-mode)
-      (xah-show-in-desktop)
-    (let ((dir (file-name-directory (buffer-file-name (buffer-base-buffer)))))
-      (w32-shell-execute "explore"
-                         (replace-regexp-in-string "/" "\\" dir t t)))))
-
 (defun gina-org-jump-to-current-clock ()
   "Interactive version of 'org-clock-jump-to-current-clock."
   (interactive)
@@ -81,12 +54,9 @@
 _b_: back (pop global mark)
 _c_: jump to Current clock
 _d_: toggle debug on error
-_e_: open explorer
 _i_: clock In to recent task
-_l_: open case list
 _r_: refile scans/downloads
 _s_: iSearch forward
-_t_: open Time sheet
 _u_: bury buffer
 _v_: toggle visual line mode
 "
@@ -94,12 +64,9 @@ _v_: toggle visual line mode
   ("b" pop-global-mark :exit nil)
   ("c" gina-org-jump-to-current-clock)
   ("d" toggle-debug-on-error)
-  ("e" gina-open-explorer)
   ("i" gina-clock-in-to-recent-task)
-  ("l" gina-open-case-list)
   ("r" gina-refile)
   ("s" isearch-forward)
-  ("t" gina-open-current-timesheet)
   ("u" bury-buffer)
   ("v" visual-line-mode))
 ;;; gina-launcher.el ends here
