56d420df3a98b474eb129d8c77881fbcb0e83c6e
Gina White <ginabythebay@gmail.com>
2021-11-28 09:40:21 -0800

    fix package-archives nongnu typo

--- init.el
+++ init.el
@@ -134,7 +134,7 @@ the syntax class ')'."
 ;;(setq package-enable-at-startup nil)
 
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-                         ("nongnu" . " https://elpa.nongnu.org/nongnu/")
+                         ("nongnu" . "https://elpa.nongnu.org/nongnu/")
                          ("melpa" . "https://melpa.org/packages/")))
 (setq package-archive-priorities
       '(("gnu" . 30)
