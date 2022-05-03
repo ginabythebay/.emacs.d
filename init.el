18898043eff1c9386c09eb901c504f293b09a4f6
Gina White <gina@PC-Ian.DOERMarine.local>
2022-05-03 07:37:40 -0700

    feeds and speeds

--- init.el
+++ init.el
@@ -65,6 +65,12 @@
 
 (winner-mode 1)
 
+;; See http://trey-jackson.blogspot.com/2010/04/emacs-tip-36-abort-minibuffer-when.html
+(defun stop-using-minibuffer ()
+  "kill the minibuffer"
+  (when (and (>= (recursion-depth) 1) (active-minibuffer-window))
+    (abort-recursive-edit)))
+(add-hook 'mouse-leave-buffer-hook 'stop-using-minibuffer)
 
 (if (eq system-type 'gnu/linux)
     (progn
