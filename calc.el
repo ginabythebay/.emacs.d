eac1b4022eed59a9f8fde63cddfeb8bd2fa1587f
Gina White <gina@PC-Ian.DOERMarine.local>
2022-08-09 16:17:38 -0700

    initial calc.el, sort of

--- calc.el
+++ calc.el
@@ -1,3 +1,8 @@
 (setq math-additional-units
       '(
         (usd nil "United States Dollar")))
+
+(defun gaw-quick-calc ()
+  "Support a popup quick calculator, meant to be called from the OS."
+  (interactive)
+  (select-frame-set-input-focus (selected-frame)))
