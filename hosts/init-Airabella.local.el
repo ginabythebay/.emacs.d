9ff4037f9f59fab46c4a0ed0c9b7b6bbe331a5bf
Gina White <ginabythebay@gmail.com>
2023-09-24 16:27:27 -0700

    add placeholder Airabella file

---
+++ hosts/init-Airabella.local.el
@@ -0,0 +1,11 @@
+;;; package --- Summary --- Gina Whites Emacs initialization for home laptop
+
+;;; Commentary:
+;;; Stuff here
+
+;;; Code:
+
+
+
+(provide 'init-Airabella.local)
+;;; init-Airabella.local.el ends here
