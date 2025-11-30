bd02436d68b4bd92c09662cf3a593ec729cc48b9
Gina White <ginabythebay@gmail.com>
2025-11-30 11:42:40 +0100

    some default emacs files for airabella

--- hosts/init-Airabella.local.el
+++ hosts/init-Airabella.local.el
@@ -5,6 +5,16 @@
 
 ;;; Code:
 
+(require 'gina-launcher)
+(customize-set-variable
+ 'gina-default-buffers
+ '((shells  "/sys" "/scratch" "/scan" "/music" "/pinky-src" "/pinky" "/boltbarn" "/blog")
+   (files
+    "~/Source/278"
+    "~/Source/DOER/snippets/snippets.org"
+    "~/finances/inbox"
+    "~/.emacs.d/init.el"
+    "~/.emacs.d/hosts/init-octavia.el")))
 
 
 (provide 'init-Airabella.local)
